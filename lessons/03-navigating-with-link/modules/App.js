a2c8266bec51a1a77be70e6c45a86ba8208f91f0
Mauricio Bonetti <bonetti.mauricio@gmail.com>
2017-01-23 16:18:25 +0000

    navigating-with-link

--- lessons/03-navigating-with-link/modules/App.js
+++ lessons/03-navigating-with-link/modules/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from "react-router"
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+    <div>
+      <div>Hello, React Router!</div>
+      <ul role="nav">
+        <Link to="/about">About</Link>
+        <Link to="/repos">Repos</Link>
+      </ul>
+    </div>
+    )
   }
 })
